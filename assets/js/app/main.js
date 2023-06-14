431479a329111776e34d8970212c170acf8943fe
Michael Chadwick <michael.chadwick@gmail.com>
2023-06-14 11:24:58 -0700

    moved dom stuff to its own js file

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -13,31 +13,6 @@ SoundLister.shuffleMode = false // for now, only 2 modes
 
 SoundLister.config = {}
 
-SoundLister.dom = {}
-SoundLister.dom.audioPlayerContainer = document.getElementById('audio-player-container')
-SoundLister.dom.audio = document.querySelector('audio')
-SoundLister.dom.currentTrackName = document.querySelector('#track-current-name span')
-SoundLister.dom.playButton = document.getElementById('play-icon')
-SoundLister.dom.playButtonIcon = document.querySelector('#play-icon i')
-SoundLister.dom.seekSlider = document.getElementById('seek-slider')
-SoundLister.dom.volumeSlider = document.getElementById('volume-slider')
-SoundLister.dom.muteButton = document.getElementById('mute-icon')
-SoundLister.dom.muteButtonIcon = document.querySelector('#mute-icon i')
-SoundLister.dom.prevButton = document.getElementById('backward')
-SoundLister.dom.repeatButton = document.getElementById('repeat-mode')
-SoundLister.dom.shuffleButton = document.getElementById('shuffle-mode')
-SoundLister.dom.nextButton = document.getElementById('forward')
-SoundLister.dom.collDisplay = document.getElementById('coll-display')
-SoundLister.dom.collDropdown = document.querySelector('#collections select')
-// STUB:
-// SoundLister.dom.collNative = document.querySelector('#collections select.selectNative')
-// SoundLister.dom.collCustom = document.querySelector('#collections .selectCustom')
-SoundLister.dom.loadMessage = document.getElementById('load-message')
-SoundLister.dom.loadAnimation = document.getElementById('load-animation')
-SoundLister.dom.progressText = document.querySelector('#progress-bar .progress__text')
-SoundLister.dom.progressBar = document.querySelector('#progress-bar .progress__bar')
-SoundLister.dom.playlist = document.getElementById('playlist')
-
 /* ********************************* */
 /* public functions                  */
 /* ********************************* */
